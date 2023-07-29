d2af91e4bd17223b908a283b8cc021052cbf3727
Emre-Kahveci <aominemre@gmail.com>
2023-07-29 18:46:03 +0300

    getting offsets at runtime

--- cs-go-trigger-bot.py
+++ cs-go-trigger-bot.py
@@ -1,18 +1,19 @@
 import pymem
 from time import sleep
 import win32api
+import requests
 
-# Offsets
-offsets = {
-    'localPlayer': 0xDEA98C,
-    'entityList': 0x4DFFF7C,
-    'forceAttack': 0x322DDE8,
-    'health': 0x100,
-    'teamNum': 0xF4,
-    'crosshairId': 0x11838 
-}
+def get_offset(url = "https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.json"):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+        json_data = response.json()
+        return json_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while fetching the JSON file: {e}")
+        return None
 
-def triggerBot() -> None:
+def triggerBot(offsets = get_offset()) -> None:
     sleep(.01)
     pm = pymem.Pymem('csgo.exe') # find csgo.exe
     
@@ -24,33 +25,32 @@ def triggerBot() -> None:
         if win32api.GetAsyncKeyState(0x58):
             continue
 
-        localPlayer = pm.read_uint(client + offsets["localPlayer"])
-        localHealth = pm.read_int(localPlayer + offsets["health"])
+        localPlayer = pm.read_uint(client + offsets["signatures"]["dwLocalPlayer"])
 
         # if the local player is dead
-        if not localHealth:
+        if not pm.read_int(localPlayer + offsets["netvars"]["m_iHealth"]):
             continue
 
-        crosshairId = pm.read_int(localPlayer + offsets['crosshairId'])
+        crosshairId = pm.read_int(localPlayer + offsets["netvars"]['m_iCrosshairId'])
 
         # competitors take values between 1 and 64
         if not crosshairId or crosshairId > 64:
             continue
 
-        player = pm.read_uint(client + offsets['entityList'] + (crosshairId - 1) * 0x10)
+        player = pm.read_uint(client + offsets["signatures"]['dwEntityList'] + (crosshairId - 1) * 0x10)
 
         # if the opponent is alive
-        if not pm.read_int(player + offsets['health']):
+        if not pm.read_int(player + offsets["netvars"]["m_iHealth"]):
             continue
 
         # If not from your team
-        if pm.read_uint(player + offsets["teamNum"]) == pm.read_uint(localPlayer + offsets["teamNum"]):
+        if pm.read_uint(player + offsets["netvars"]["m_iTeamNum"]) == pm.read_uint(localPlayer + offsets["netvars"]["m_iTeamNum"]):
             continue
         
         # shoot
-        pm.write_uint(client + offsets['forceAttack'], 6)
+        pm.write_uint(client + offsets["signatures"]['dwForceAttack'], 6)
         sleep(.05)
-        pm.write_uint(client + offsets['forceAttack'], 4)
+        pm.write_uint(client + offsets["signatures"]['dwForceAttack'], 4)
 
 
 if __name__ == "__main__":
